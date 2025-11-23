5bea097cab63dcb05ce93e35d2ecbbb99ba7e826
Yarchik07 <volokitinyaroslav07@gmail.com>
2025-11-23 06:40:20 +0300

    a

--- voice.py
+++ voice.py
@@ -1,35 +1,26 @@
 import torch
 import sounddevice as sd
 import time
-
 language = "ru"
 model_id = "ru_v3"
 sample_rate = 48000
-speaker = "baya"
+speaker = "aidar"
 put_accent = True
 put_yo = True
 device = torch.device("cpu")  # cpu или gpu
 text = "Илья лох!"
-
-model, _ = torch.hub.load(
-    repo_or_dir="snakers4/silero-models",
-    model="silero_tts",
-    language=language,
-    speaker=model_id,
-)
+model, _ = torch.hub.load(repo_or_dir="snakers4/silero-models",
+                          model="silero_tts",
+                          language=language,
+                          speaker=model_id,)
 model.to(device)
 
-
-
-# воспроизводим
 def va_speak(what: str):
-    audio = model.apply_tts(
-        text=text,
-        speaker=speaker,
-        sample_rate=sample_rate,
-        put_accent=put_accent,
-        put_yo=put_yo,)
-
-sd.play(audio, sample_rate)
-time.sleep(len(audio) / sample_rate)
-sd.stop()
+    audio = model.apply_tts(text=text,
+                            speaker=speaker,
+                            sample_rate=sample_rate,
+                            put_accent=put_accent,
+                            put_yo=put_yo,)
+    sd.play(audio, sample_rate * 1.05)
+    time.sleep((len(audio) / sample_rate)+0.5)
+    sd.stop()
