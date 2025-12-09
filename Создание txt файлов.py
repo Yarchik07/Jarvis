3422c1ee554eff983c57a1dd9a7582fb57e11513
Yarchik07 <volokitinyaroslav07@gmail.com>
2025-12-09 15:21:24 +0300

    Delete Создание txt файлов.py

--- Создание txt файлов.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def quick_txt(name):#сюда голосовой ввод
-    path = os.path.join(os.path.expanduser("~"), "Documents", name + '.txt')
-    with open(path, 'w', encoding='utf-8') as f:
-        content = #сюда голосовой ввод
-        f.write(content)
-    return path
