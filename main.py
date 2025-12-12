72be06d5af028eb52d98af022f2ec617bcfd31a1
ShopchaS <bkucnucoup@mail.ru>
2025-12-12 19:37:57 +0300

    pep8

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-"""Это программа для управления громкостью звука в Windows. 
-Она позволяет регулировать уровень звука, включать и выключать его через простое текстовое меню."""
+"""
+Программа для управления громкостью звука в Windows.
+Позволяет регулировать уровень звука, включать и выключать его
+через простое текстовое меню.
+"""
+
 from datetime import date
 from sound import Sound
 
-print("")
-print("Windows Sound Manager (Python 3)")
-print("Copyright (c) 2014 - %d | Paradoxis" % date.today().year)
-print("")
 
-while True:
+def print_header():
+    """Выводит заголовок программы."""
+    print("")
+    print("Windows Sound Manager (Python 3)")
+    print("Copyright (c) 2014 - %d | Paradoxis" % date.today().year)
+    print("")
+
+
+def print_menu():
+    """Выводит меню выбора опций."""
     print("Choose an option:")
     print("1] Mute / Unmute volume")
     print("2] Increase volume")
@@ -18,40 +27,60 @@ while True:
     print("6] Set volume to ...")
     print("7] Print sound settings")
     print("8] Quit")
-    option = input("> ")
+
+
+def print_sound_settings():
+    """Выводит текущие настройки звука."""
+    print("Current volume | %s" % str(Sound.current_volume()))
+    print("Sound muted    | %s" % str(Sound.is_muted()))
+    print("----------------------")
     print("")
 
-    if (option == "1"):
-        Sound.mute()
-        continue
 
-    if (option == "2"):
-        Sound.volume_up()
-        continue
+def main():
+    """Основная функция программы."""
+    print_header()
 
-    if (option == "3"):
-        Sound.volume_down()
-        continue
-
-    if (option == "4"):
-        Sound.volume_min()
-        continue
-
-    if (option == "5"):
-        Sound.volume_max()
-        continue
-
-    if (option == "6"):
-        volume = int(input("Volume (0 - 100): "))
-        Sound.volume_set(volume)
-        continue
-
-    if (option == "7"):
-        print("Current volume | %s" % str(Sound.current_volume()))
-        print("Sound muted    | %s" % str(Sound.is_muted()))
-        print("----------------------")
+    while True:
+        print_menu()
+        option = input("> ")
         print("")
-        continue
 
-    if (option == "8"):
-        exit(0)
+        if option == "1":
+            Sound.mute()
+            continue
+
+        if option == "2":
+            Sound.volume_up()
+            continue
+
+        if option == "3":
+            Sound.volume_down()
+            continue
+
+        if option == "4":
+            Sound.volume_min()
+            continue
+
+        if option == "5":
+            Sound.volume_max()
+            continue
+
+        if option == "6":
+            try:
+                volume = int(input("Volume (0 - 100): "))
+                Sound.volume_set(volume)
+            except ValueError:
+                print("Error: Please enter a valid number.")
+            continue
+
+        if option == "7":
+            print_sound_settings()
+            continue
+
+        if option == "8":
+            exit(0)
+
+
+if __name__ == "__main__":
+    main()
